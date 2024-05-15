456b35860f985485500039f5b202380233772e60
shan251197 <samjacksj1997@gmail.com>
2024-05-15 13:16:46 +0530

    sales & items added in data4

---
+++ functions/data4/items.js
@@ -0,0 +1,49 @@
+const items = [
+  {
+    id: 2001,
+    name: "coke",
+    price: 100,
+  },
+  {
+    id: 2002,
+    name: "Rum",
+    price: 500,
+  },
+  {
+    id: 2003,
+    name: "Brandy",
+    price: 400,
+  },
+  {
+    id: 2004,
+    name: "Chicken wings",
+    price: 500,
+  },
+  {
+    id: 2005,
+    name: "Burgers",
+    price: 400,
+  },
+  {
+    id: 2006,
+    name: "French fries",
+    price: 200,
+  },
+  {
+    id: 2007,
+    name: "Pizza",
+    price: 800,
+  },
+  {
+    id: 2008,
+    name: "Sandwiches",
+    price: 300,
+  },
+  {
+    id: 2009,
+    name: "Popcorn",
+    price: 300,
+  },
+];
+
+module.exports = items;
---
+++ functions/data4/sale.js
@@ -0,0 +1,36 @@
+const sales = [
+  {
+    id: 1,
+    bill_amount: 1000,
+    amount_paid: 1000,
+    customer_id: 101,
+    customer_name: "shan",
+    mobile_number: "9600774861",
+    customer: null,
+    store_id: 1001,
+    date_time: "2024-05-15 18:06:00",
+    points_gained: 10,
+    points_redeemed: 0,
+    items: [
+      {
+        id: 2001,
+        name: "coke",
+        price: 100,
+      },
+      {
+        id: 2002,
+        name: "Rum",
+        price: 500,
+      },
+      {
+        id: 2003,
+        name: "Brandy",
+        price: 400,
+      },
+    ],
+  },
+];
+
+module.exports = sales;
+
+//
