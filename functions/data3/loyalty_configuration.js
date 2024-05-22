18d9fc6348d1d3cd87402387670be615bbc28e88
shan251197 <samjacksj1997@gmail.com>
2024-05-22 11:22:02 +0530

    loyalty config changes

--- functions/data3/loyalty_configuration.js
+++ functions/data3/loyalty_configuration.js
@@ -10,22 +10,27 @@ const loyaltyConfguration = [
         points: 300,
         discount_percentage: 10,
         max_discount: 1000,
+        name: "10% discount",
       },
+
       {
         id: 2,
         points: 500,
         discount_percentage: 20,
         max_discount: 2000,
+        name: "20% discount",
       },
       {
         id: 3,
         points: 300,
+        name: "Flat Rs.1000 discount",
         discount_amount: 1000,
       },
       {
         id: 4,
         points: 100,
         free_items: [2001],
+        name: "Free item",
       },
     ],
   },
